a40625116ad55c5fde586c0098403272e15ef771
Sean Bellows <seanbellows@gmail.com>
2016-09-30 17:47:21 -0400

    changed url path for symbol-defs.svg file to point relatively

--- resources/assets/js/jquery.svgIconPicker.ajax.js
+++ resources/assets/js/jquery.svgIconPicker.ajax.js
@@ -2,7 +2,7 @@
   /**
    * AJAX in the icon SVG sprite
    */
-  $.get("/app/plugins/chamber-cms/resources/assets/icons/sprite.svg", function(data) {
+  $.get("../icons/sprite.svg", function(data) {
     var div = document.createElement("div");
     div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
     document.body.insertBefore(div, document.body.childNodes[0]);
