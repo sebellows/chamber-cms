f81fdc7bc417ac8fbf8a0ecaab170cff97b205a5
Joel Howard <joelhowa@umflint.edu>
2016-11-07 22:55:11 -0500

    added start of short tags

--- app/shortcodes.php
+++ app/shortcodes.php
@@ -3,3 +3,18 @@
 namespace Chamber\Plugin;
 
 /** @var \Oni\Framework\Shortcode $shortcode */
+
+// person shortcode
+$shortcode->add(
+    'person', function ($person_id) {
+        echo $person_id; 
+
+});
+
+
+// attraction shortcode
+$shortcode->add(
+    'attraction', function ($attraction_id) {
+
+
+});
