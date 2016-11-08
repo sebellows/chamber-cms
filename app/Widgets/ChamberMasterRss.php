49c8f0502cde2af70d782d5b2e623c8b6b573e77
Sean Bellows <seanbellows@gmail.com>
2016-11-08 12:10:24 -0500

    add before_widget and after_widget arguments, modify markup to mimic other current widget classes

--- app/Widgets/ChamberMasterRss.php
+++ app/Widgets/ChamberMasterRss.php
@@ -15,7 +15,7 @@ class ChamberMasterRss extends \WP_Widget {
         $this->widget_id    = Helper::slugify($this->widget_class);
         $this->widget_name  = 'Chamber Master Upcoming Events RSS';
         $this->rss_feed_url = 'https://member.flintandgenesee.org/Feed/rss/UpcomingEvents.rss';
-        $this->description_length = 50;
+        $this->description_length = 120;
         $this->display_items = 3;
 
         $params = [
@@ -35,36 +35,48 @@ class ChamberMasterRss extends \WP_Widget {
         $rssFeed = fetch_feed($this->rss_feed_url);
         $rssArr = $rssFeed->get_items(0, $this->display_items);
 
-        $output = '<section id="events" class="widget widget-event-listing">' .
-        '<h3 class="widget-title">Upcoming Events</h3>' .
-        '<ul>';
+        ?>
 
+        <?php echo $args['before_widget']; ?>
 
-        foreach ( $rssArr as $rssItem ) {
+        <h3 class="widget-title">Upcoming Events</h3>
+
+        <ul>
+
+        <?php foreach ( $rssArr as $rssItem ) {
 
             // perform split to get date and title
             list($date, $title) = explode(' - ', $rssItem->get_title());
+?>
+            <li class="event">
+                <time class="event-time">
+                    <span class="event-date">
+                        <span class="event-day"><?= date_format(date_create($date), 'd'); ?></span>
+                        <span class="event-month"><?= date_format(date_create($date), 'M'); ?></span>
+                        <span class="event-year"><?= date_format(date_create($date), 'Y'); ?></span>
+                    </span>
+                </time>
 
-            $output .= '<li class="event">' .
-            '<time class="event-date">';
+                <div class="event-desc">
+                    <h4 class="event-desc-header"><?= $title; ?></h4>
 
-            $output .= '<span class="event-month">' . date_format(date_create($date), 'M') . '</span>' .
-            '<span class="event-day">' . date_format(date_create($date), 'd') . '</span>' .
-            '</time>';
+                    <div class="event-desc-detail">
+                        <?= substr(strip_tags($rssItem->get_description()), 0, $this->description_length); ?>
+                    </div>
+                    <a href="<?= $rssItem->get_permalink(); ?>" class="small hollow rsvp button">RSVP &amp; Details</a>
+                </div>
 
-            $output .= '<div class="event-desc">' .
-            '<h4 class="event-desc-header">' . $title . '</h4>' .
-            '<span class="event-desc-detail"><span class="event-desc-time"></span>' . substr(strip_tags($rssItem->get_description()), 0, $this->description_length) . '...</span>' .
-            '<a href="' . $rssItem->get_permalink() .'" class="small button rsvp">RSVP &amp; Details</a>' .
-            '</div>' .
-            '</li>';
-        }
+            </li>
 
-        $output .= '</ul>';
-        $output .= '</section>';
+            <?php } ?>
 
-        echo $output;
-        //return view('@Chamber/widgets/chamber_master_rss.twig'); ~ not sure why this isn't working.  shitty documentation on herbert...
+        </ul>
+
+        <?php echo $args['after_widget']; ?>
+
+        <?php wp_reset_postdata(); ?>
+
+        <?php
 
     }
 
